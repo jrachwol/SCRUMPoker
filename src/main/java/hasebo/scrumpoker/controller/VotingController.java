a8bbf0cc9bab6bb7a250c9fa00be79db72327589
janusz.rachwol <janusz.rachwol@gmail.com>
2024-04-15 12:25:20 +0200

    Poprawia oznaczanie wartości głosu na radio button

--- src/main/java/hasebo/scrumpoker/controller/VotingController.java
+++ src/main/java/hasebo/scrumpoker/controller/VotingController.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
